f8d135e7b54891717d3273e951dff1dc8fb404c0
guanliming <guanliming@mobanker.com>
2015-05-13 16:25:20 +0800

    add test

---
+++ test/test-action/src/main/java/com/qianlong/filter/LoginFilter.java
@@ -0,0 +1,39 @@
+package com.qianlong.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author 管黎明
+ * 
+ *         All rights reserved.
+ */
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(final ServletRequest request,
+			final ServletResponse response, final FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(final FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
