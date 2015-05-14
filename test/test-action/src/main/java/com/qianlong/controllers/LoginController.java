6a4e0a32da24e670016d1645fd4e564090c991ec
guanliming <guanliming@mobanker.com>
2015-05-14 13:53:52 +0800

    snapshot1

---
+++ test/test-action/src/main/java/com/qianlong/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package com.qianlong.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author 管黎明
+ *
+ *         All rights reserved.
+ */
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public ModelAndView login(){
+		return new ModelAndView("forward:/WEB-INF/views/login.jsp");
+	}
+	
+	
+
+}
--- test/test-action/src/main/java/com/qianlong/filter/LoginFilter.java
+++ test/test-action/src/main/java/com/qianlong/filter/LoginFilter.java
@@ -3,6 +3,8 @@ package com.qianlong.filter;
 import static com.qianlong.constants.SystemConstant.SESSION_LOGIN_NAME;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +27,9 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Slf4j
 public class LoginFilter implements Filter {
+
+	private static final List<String> ignoreURI = Arrays.asList("login");
+
 	@Override
 	public void destroy() {
 		//
@@ -33,7 +39,7 @@ public class LoginFilter implements Filter {
 	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
 			throws IOException, ServletException {
 		final HttpServletRequest hsreq = (HttpServletRequest) request;
-//		 final HttpServletResponse hsresp = (HttpServletResponse) response;
+		final HttpServletResponse hsresp = (HttpServletResponse) response;
 		final String name = hsreq.getParameter("name");
 		final HttpSession session = hsreq.getSession();
 		final String loginNameFromSession = String.valueOf(session.getAttribute(SESSION_LOGIN_NAME));
@@ -41,8 +47,15 @@ public class LoginFilter implements Filter {
 			chain.doFilter(request, response);
 			return;
 		}
-		hsreq.getRequestDispatcher("/index.html").forward(request, response);
-		log.info("还没登陆啊！！");
+		final String uri = hsreq.getRequestURI();
+		for (final String eachURI : ignoreURI) {
+			if (uri.indexOf(eachURI) != -1) {
+				chain.doFilter(request, response);
+				return;
+			}
+		}
+//		hsresp.sendRedirect("/test-action/login");
+		hsreq.getRequestDispatcher("/login").forward(request, response);
 	}
 
 	@Override
