296c9c572ac0a0c3a3bb7bff91dbcd912a41a94e
shadow <gdmt4@126.com>
2015-11-09 23:16:16 +0800

    all2

---
+++ test/mytest/src/main/java/com/shadow/mytest/T1.java
@@ -0,0 +1,10 @@
+package com.shadow.mytest;
+
+public class T1 {
+
+	public String print(){
+		System.out.println("a");
+		return "a";
+	}
+
+}
---
+++ test/test-action/src/test/java/com/qianlong/controllers/Md5Test.java
@@ -0,0 +1,97 @@
+package com.qianlong.controllers;
+
+import java.security.MessageDigest;
+
+import org.junit.Test;
+
+public class Md5Test {
+
+	@Test
+	public void test() {
+		// System.out.print(MD5_test.MD5("b"));
+
+		String s = new String("a");
+		System.out.println("原始：" + s);
+		System.out.println("MD5后：" + MD5_test.MD5(s));
+		System.out.println("MD5后再加密：" + MD5andKL.KL(MD5_test.MD5(s)));
+		System.out.println("解密为MD5后的：" + MD5andKL.JM(MD5andKL.KL(MD5_test.MD5(s))));
+	}
+
+}
+
+class MD5_test {
+	public final static String MD5(String s) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		try {
+			byte[] strTemp = "hello you !".getBytes();
+			MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+			mdTemp.update(strTemp);
+			byte[] md = mdTemp.digest();
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			return new String(str);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
+
+class MD5andKL {
+
+	public static String JM(String inStr) {
+		char[] a = inStr.toCharArray();
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (char) (a[i] ^ 't');
+		}
+		String k = new String(a);
+		return k;
+	}
+
+	public static String KL(String inStr) {
+		// String s = new String(inStr);
+		char[] a = inStr.toCharArray();
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (char) (a[i] ^ 't');
+		}
+		String s = new String(a);
+		return s;
+	}
+
+	// MD5加码。32位
+	public static String MD5(String inStr) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			return "";
+		}
+		char[] charArray = inStr.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++) {
+			byteArray[i] = (byte) charArray[i];
+		}
+
+		byte[] md5Bytes = md5.digest(byteArray);
+
+		StringBuffer hexValue = new StringBuffer();
+
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = (md5Bytes[i]) & 0xff;
+			if (val < 16) {
+				hexValue.append("0");
+			}
+			hexValue.append(Integer.toHexString(val));
+		}
+
+		return hexValue.toString();
+	}
+}
