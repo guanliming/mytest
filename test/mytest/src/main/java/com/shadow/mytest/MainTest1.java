7f8b5f4483448bd5b6a9c2cf98085eda938eaa97
shadow <gdmt4@126.com>
2015-11-09 23:14:47 +0800

    all

--- test/mytest/src/main/java/com/shadow/mytest/MainTest1.java
+++ test/mytest/src/main/java/com/shadow/mytest/MainTest1.java
@@ -4,49 +4,86 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.crypto.SecretKey;
+
+import lombok.Data;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * @author 管黎明
- * 
+ *
  *         All rights reserved.
  */
 public class MainTest1 {
-	public static void main(final String[] args) throws URISyntaxException {
+	public static void main(final String[] args) throws URISyntaxException, Exception {
 		final List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-		pairs.add(new BasicNameValuePair("param1", "a1"));
-		pairs.add(new BasicNameValuePair("param2", "a2"));
-//		final URI uri = new URI("http", null, "localhost", 8080, "/test-action/http/a1", URLEncodedUtils.format(pairs,
-//				Charset.forName("UTF-8")), null);
+		String s1 = "a1";
+		String s2 = "a2";
+		Params params = new Params();
+		params.setParam1(s1);
+
+		// pairs.add(new BasicNameValuePair("param1", s1));
+		// pairs.add(new BasicNameValuePair("param2", s2));
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update((s1 + s2).getBytes());
+			byte[] tmp = md5.digest();
+			StringBuilder sb = new StringBuilder();
+			for (byte b : tmp) {
+				sb.append(Integer.toHexString(b & 0xFF));
+			}
+			params.setMd5(sb.toString());
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+		}
+
+		// final URI uri = new URI("http", null, "localhost", 8080,
+		// "/test-action/http/a1", URLEncodedUtils.format(pairs,
+		// Charset.forName("UTF-8")), null);
 		final URI uri = new URI("http", null, "localhost", 8080, "/test-action/http/a1", null, null);
 		final HttpPost post = new HttpPost(uri);
-//		final Header header2 = new BasicHeader("ACCEPT","application/json");
-		final Header header2 = new BasicHeader("Content-Type","text/html");
-		post.setHeaders(new Header[]{header2});
-		post.setEntity(new UrlEncodedFormEntity(pairs,Charset.forName("UTF-8")));
+		// final Header header2 = new BasicHeader("ACCEPT","application/json");
+		final Header header2 = new BasicHeader("Content-Type", "text/html");
+		post.setHeaders(new Header[] { header2 });
+		EncrypDES3 des = new EncrypDES3();
+		params.setKey(des.getDeskey());
+		params.setParam2(new String(des.Encrytor(s2)));
+		post.setEntity(new StringEntity((JSONObject.toJSONString(params))));
 		try {
 			final CloseableHttpResponse response = HttpClientBuilder.create()
 					.setDefaultRequestConfig(RequestConfig.custom().build()).build().execute(post);
 			final HttpEntity entity = response.getEntity();
-			System.out.println(EntityUtils.toString(entity,Charset.forName("utf-8")));
+			System.out.println(EntityUtils.toString(entity, Charset.forName("utf-8")));
 		} catch (final ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 	}
+}
+
+@Data
+class Params {
+	private SecretKey key;
+	private String md5;
+	private String param1;
+	private String param2;
 }
--- test/test-action/src/main/java/com/qianlong/controllers/HttpTestController.java
+++ test/test-action/src/main/java/com/qianlong/controllers/HttpTestController.java
@@ -1,23 +1,28 @@
 package com.qianlong.controllers;
 
-import java.util.List;
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 import lombok.Data;
 import lombok.Getter;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.qianlong.BorrowEntity;
 import com.qianlong.biz.IBorrowBiz;
 
 /**
  * @author 管黎明
- * 
+ *
  *         All rights reserved.
  */
 @Controller
@@ -38,9 +43,27 @@ public class HttpTestController {
 
 	@RequestMapping(value = "/a1", method = { RequestMethod.POST, RequestMethod.GET })
 	@ResponseBody
-	public String a1(final TransferParams params) {
-		final List<BorrowEntity> entity = borrowBiz.query(10);
+	public String a1(@RequestBody final String params) throws Exception {
+//		final List<BorrowEntity> entity = borrowBiz.query(10);
+		TransferParams param = JSON.parseObject(params, TransferParams.class);
+
+		 Cipher c = Cipher.getInstance("DESede");
+		 c.init(Cipher.ENCRYPT_MODE, param.getKey());
+	        byte[] src = param.getParam2().getBytes();
+	        // 加密，结果保存进cipherByte
+	      byte[]  cipherByte = c.doFinal(src);
+	      param.setParam2(new String(cipherByte));
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		md5.update((param.getParam1()+param.getParam2()).getBytes());
+		byte[] tmp =md5.digest();
+		StringBuilder sb = new StringBuilder();
+		for(byte b:tmp){
+			sb.append(Integer.toHexString(b&0xFF));
+		}
+		if(StringUtils.equals(param.getMd5(), sb.toString())){
 		return JSONObject.toJSONString(new MyResponse("OK", "fejaijfi"));
+		}
+		throw new RuntimeException("djfl");
 	}
 
 	// @ModelAttribute
@@ -52,9 +75,10 @@ public class HttpTestController {
 
 @Data
 class TransferParams {
+	private SecretKey key;
+	private String md5;
 	private String param1;
 	private String param2;
-
 	public TransferParams() {
 	}
 }
